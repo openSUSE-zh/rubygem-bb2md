8e270a064aa478b1f2ba28a13fd6a991104e4e85
Marguerite Su <i@marguerite.su>
2017-09-11 05:25:09 +0000

    finish quote

--- lib/bb2md/parser.rb
+++ lib/bb2md/parser.rb
@@ -3,5 +3,13 @@ module BB2MD
     def initialize(text)
       @text = text
     end
+
+    def parse
+      @text = BB2MD::Quote.new(@text).text
+    end
   end
 end
+
+require './quote.rb'
+
+BB2MD::Parser.new("").parse
